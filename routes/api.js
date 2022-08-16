b3b38b3d46fa641ff2c1ab7542a7ef53abfe123b
amekiri13 <admin@amekiri.com>
2022-08-16 18:07:17 +0800

    代码修正

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {queryBasicInfo,queryBasicInfoCN} = require('../web/basic');
+const {queryBasicInfo} = require('../web/basic');
 const {queryDetailInfo} = require("../web/details");
 const {queryAbyssInfo} = require("../web/abyss");
 
